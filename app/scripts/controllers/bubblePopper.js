4859da778ddcf00f2239c660ae632dd65118e469
Andy M <andy.dude@gmail.com>
2014-12-02 20:40:53 -0600

    Added bubble popper page

--- app/scripts/controllers/bubblePopper.js
+++ app/scripts/controllers/bubblePopper.js
@@ -8,6 +8,120 @@
  * Controller of the portfolioApp
  */
 angular.module('portfolioApp')
-  .controller('bubblePopperCtrl', function ($scope) {
-    
-  });
+  .controller('bubblePopperCtrl', ['bubbleService', '$scope', function (bubbleService, $scope) {
+    var bubbles = [];
+    var canvas = angular.element('#canvas')[0];
+    var context = canvas.getContext('2d'),
+        animationFrameId;
+
+    var Bubble = bubbleService.Bubble,
+        Vector2D = bubbleService.Vector2D;
+
+
+    $scope.canvasClick= function canvasClick(e){
+        var position = getCursorPosition(e);
+
+        for (var i = bubbles.length - 1; i >=0; i--){
+            var bubble = bubbles[i];
+            if (bubble.state == Bubble.States.Active){
+                if (bubble.isPointOnBubble(position)){
+                    bubble.state = Bubble.States.Popping;
+                    playAudio();
+                    break;
+                }
+            }
+        }
+    };
+
+
+    var playIndex = 0;
+    function playAudio(){
+        var audio = document.getElementById('popAudio' + playIndex);
+        audio.play();
+        playIndex++;
+        if (playIndex >= 4){
+            playIndex = 0;
+        }
+    }
+
+    function getCursorPosition(e) {
+        var x;
+        var y;
+        if (e.pageX || e.pageY) {
+            x = e.pageX;
+            y = e.pageY;
+        } else {
+            x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+
+        x -= e.target.offsetLeft;
+        y -= e.target.offsetTop;
+
+        return {
+            x: x,
+            y: y
+        };
+    }
+
+    function move(){
+        context.clearRect(0,0,canvas.width, canvas.height);
+        var indexToRemove = -1;
+        for(var i =0; i < bubbles.length; i++){
+            var bubble = bubbles[i];
+            if (bubble.state == Bubble.States.Active){
+                bubble.pos2D = bubble.pos2D.addScaled(bubble.velo2D, 1);
+                if(bubble.pos2D.x > canvas.width + bubble.radius){
+                    bubble.x = 0 - bubble.radius;
+                }
+                if(bubble.pos2D.x < - bubble.radius){
+                    bubble.x = canvas.width + bubble.radius;
+                }
+                if(bubble.pos2D.y > canvas.height + bubble.radius){
+                    bubble.y = 0 - bubble.radius;
+                }
+                if(bubble.pos2D.y < - bubble.radius){
+                    bubble.y = canvas.height + bubble.radius;
+                }
+            }
+            else if (bubble.state === Bubble.States.Inactive){
+                indexToRemove = i;
+                continue;
+            }
+
+
+            bubble.draw(context);
+        }
+        if (indexToRemove >= 0){
+            bubbles.splice(indexToRemove, 1);
+        }
+
+        animationFrameId = requestAnimationFrame(move) || animationFrameId;    
+    }
+
+    $scope.init = function init(){
+        for(var i =0; i < $scope.bubbleCount; i++){
+                var radius = (Math.random() + 2)* 20;
+                var color = 'hsl(' + Math.random() * 360 + ',100%, 50%)';
+                var bubble = new Bubble(radius, color, 1, 0, true);
+                bubble.pos2D = new Vector2D(canvas.width/2, canvas.height/2);
+                bubble.velo2D = new Vector2D( (Math.random() - .5)*2, 
+                    (Math.random() - .5) *2 );
+                bubble.draw(context);
+                bubbles.push(bubble);
+            }
+
+    }
+
+    $scope.bubbleCount = 50;
+
+    $scope.init();
+
+    animationFrameId = requestAnimationFrame(move);
+
+    $scope.$on('$destroy', function onDestroy() {
+        cancelAnimationFrame(animationFrameId);
+    });
+
+
+}]);
---
+++ app/scripts/services/bubbleService.js
@@ -0,0 +1,217 @@
+'use strict';
+
+angular.module('portfolioApp').
+    factory('bubbleService', [function(){
+
+    function Particle(mass,charge){
+        if (mass == null) mass = 1;
+        if (charge == null) charge = 1;
+
+        this.mass = mass;
+        this.charge = charge;
+        this.x = 0;
+        this.y = 0;
+        this.vx = 0;
+        this.vy = 0;
+    }
+
+    Particle.prototype = {
+        get pos2D(){
+            return new Vector2D(this.x, this.y);
+        },
+        set pos2D(pos){
+            this.x = pos.x;
+            this.y = pos.y;
+        },
+        get velo2D(){
+            return new Vector2D(this.vx, this.vy);
+        },
+        set velo2D(velocity){
+            this.vx = velocity.x;
+            this.vy = velocity.y;
+        }
+    };
+
+    function Bubble(radius, color, mass, charge, gradient){
+        if (radius == null) radius = 20;
+        if (color == null) color = "#0000ff";
+        if (mass == null) mass = 1;
+        if (charge == null) charge = 0;
+        if (gradient == null) gradient = false;
+
+        this.color = color;
+        this.radius = radius;
+        this.mass = mass;
+        this.charge = charge;
+        this.gradient = gradient;
+
+        this.x = 0;
+        this.y = 0;
+        this.vx =0;
+        this.vy = 0;
+        this.state = Bubble.States.Active;
+        this.popCount = 0;
+        this.popLines = 10;
+        this.popLineLength = 10;
+    }
+
+    Bubble.States = {
+        Active : 0,
+        Popping: 1,
+        Inactive: 2
+    };
+    Bubble.PopDuration = 50;
+
+
+    Bubble.prototype = Object.create(Particle.prototype);
+    Bubble.prototype.constructor = Bubble;
+
+    Bubble.prototype.draw = function draw (context) {
+        if (this.state === Bubble.States.Inactive){
+            return;
+        }
+
+        if (this.state === Bubble.States.Active){
+
+            if (this.gradient){
+                var grad = context.createRadialGradient(this.x, this.y, 
+                    0, this.x, this.y, this.radius);
+                grad.addColorStop(0, '#ffffff');
+                grad.addColorStop(1, this.color);
+                context.fillStyle = grad;
+            }
+            else{
+                context.fillStyle = this.color;
+            }
+
+            context.beginPath();
+            context.arc(this.x, this.y, this.radius, 0, 2*Math.PI, true);
+            context.closePath();
+            context.fill();
+        }
+
+        if (this.state === Bubble.States.Popping){
+            //angles
+            var angleInDeg = 360 / this.popLines;
+            var angleInRad = angleInDeg * Math.PI / 180;
+            var currentAngle = 0;
+     
+            context.strokeStyle = this.color;
+
+            for(var line = 0 ; line < this.popLines; line++){
+                var cosX = Math.cos(currentAngle);
+                var sinY = Math.sin(currentAngle);
+                var startX = this.x + this.popCount * cosX;
+                var startY = this.y + this.popCount * sinY;
+                var endX = this.x + (this.popCount +  this.popLineLength) * cosX;
+                var endY = this.y + (this.popCount + this.popLineLength) * sinY;
+
+                context.beginPath();
+                context.moveTo(startX, startY);
+                context.lineTo(endX, endY);
+
+                context.stroke();
+                currentAngle += angleInRad;
+            }
+
+
+            this.popCount += 1;
+
+            if (this.popCount >= Bubble.PopDuration){
+                this.state = Bubble.States.Inactive;
+            }
+        }
+
+    };
+
+    Bubble.prototype.isPointOnBubble = function isPointOnBubble(point){
+        var diffX = this.x-point.x;
+        var diffY = this.y-point.y;
+        var distanceToPoint = Math.sqrt(diffX* diffX + diffY * diffY);
+        return distanceToPoint <= this.radius;
+    }
+
+
+    Bubble.prototype.onclick = function onclick(draw){
+        if (this.state == Bubble.States.Active){
+            this.state = Bubble.States.Popping;
+        }
+    }
+
+
+    function Vector2D(x, y){
+        this.x = x;
+        this.y = y; 
+    }
+
+    Vector2D.prototype ={
+        lengthSquared: function lengthSquared(){
+            return this.x* this.x + thisorm.y *this.y;
+        },
+        length: function length(){
+            return Math.sqrt(this.lengthSquared());
+        },
+        clone: function clone(){
+            return new Vector2D(this.x, this.y);
+        },
+        negate: function negate(){
+            this.x = -this.x;
+            this.y = -this.y;
+        },
+        normalize: function normalize(){
+            var length = this.length();
+            if (length > 0){
+                this.x = this.x / length;
+                this.y = this.y / length;
+            }
+
+            return length;
+        },
+        add: function add(vec){
+            return new Vector2D(this.x + vec.x, this.y + vec.y);
+        },
+        incrementBy: function incrementBy(vec){
+            this.x += vec.x;
+            this.y += vec.y;
+        },
+        subtract: function subtract(vec){
+            return new Vector2D(this.x - vec.x, this.y - vec.y);
+        },
+        decrementBy: function decrementBy(vec){
+            this.x -= vec.x;
+            this.y -= vec.y;
+        },
+        multiply: function multiply(num){
+            return new Vector2D(this.x * num, this.y * num);
+        },
+        addScaled: function addScaled(vec, num){
+            return this.add(vec.multiply(num));
+        },
+        scaleBy: function scaleBy(num){
+            this.x *= num;
+            this.y *= num;
+        },
+        dotProduct: function dotProduct(vec){
+            return this.x*vec.x + this.y * vec.y;
+        }
+
+    };
+
+
+
+    Vector2D.distance = function distance (vector1, vector2) {
+        return vector1.subtract(vector2).length();
+    };
+
+    Vector2D.angleBetween = function angleBetween(vector1, vector2){
+        return Math.acos(vector1.dotProduct(vector2)/ 
+            (vector1.length()*vector2.length()));
+    }
+
+
+    return  {
+        Bubble: Bubble,
+        Vector2D: Vector2D
+    };
+
+}]);
