a47a85d13c55adde8c2627418b9179e7a69350a2
Andy M <andy.dude@gmail.com>
2015-04-27 19:18:38 -0500

    Adding planets!

--- app/scripts/controllers/threejsPlanet.js
+++ app/scripts/controllers/threejsPlanet.js
@@ -1,56 +1,60 @@
 'use strict'
 
 angular.module('portfolioApp').controller('threejsPlanetCtrl', ['$scope',  'planetService', function($scope, planetService){
+    var scene, ambientLight, light,camera, renderer, spaceObject;
     $scope.objects = planetService.retrieveAll();
 
     $scope.selectedObject = $scope.objects[3]; //earth
 
-    var scene = new THREE.Scene();
-    var ambientLight = new THREE.AmbientLight( 0x000000 );
-    scene.add( ambientLight );
+    $scope.$watch('selectedObject', function(){
+        updateObject();
+    });
 
+    function updateObject(){
+        if (scene == null){
+            return;
+        }
+        if (spaceObject != null){
+            scene.remove(spaceObject);
+        }
+        var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
+        var material = $scope.selectedObject.getMaterial();
 
-    var light   = new THREE.DirectionalLight( 0xcccccc, 1 )
-    light.position.set(5,5,5)
-    scene.add( light )
-    light.castShadow    = true
-    light.shadowCameraNear  = 0.01
-    light.shadowCameraFar   = 15
-    light.shadowCameraFov   = 45
-    light.shadowCameraLeft  = -1
-    light.shadowCameraRight =  1
-    light.shadowCameraTop   =  1
-    light.shadowCameraBottom= -1
-    light.shadowBias    = 0.001
-    light.shadowDarkness    = 0.2
-    light.shadowMapWidth    = 1024
-    light.shadowMapHeight   = 1024
+        spaceObject = new THREE.Mesh(geometry, material);
+        
+        scene.add(spaceObject);
 
-    var camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.z = 5;
+    }
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight - 50 );
-    document.body.appendChild( renderer.domElement );
+    function initScene(){
+        scene = new THREE.Scene();
+        ambientLight = new THREE.AmbientLight( 0x000000 );
+        scene.add( ambientLight );
 
-    var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
-   // var material  = new THREE.MeshBasicMaterial();
-    //var material = new THREE.MeshBasicMaterial( { color: 0x2f4f4f });//0xff0000 } );
-    // var material = new THREE.MeshPhongMaterial({
-    //     // map :THREE.ImageUtils.loadTexture('images/planets/earthmap1k.jpg')
-    //     map: THREE.ImageUtils.loadTexture('images/planets/earthlights1k.jpg'),
-    //     //lightMap: THREE.ImageUtils.loadTexture('images/planets/earthmap1k.jpg')
-    //     //normalMap: THREE.ImageUtils.loadTexture('images/planets/earthlights1k.jpg')
-    //     emissive: '#ffffff'
-    // });
-    //material.
-    //material.map    = THREE.ImageUtils.loadTexture('images/planets/earthlights1k.jpg')
+        light   = new THREE.DirectionalLight( 0xcccccc, 1 )
+        light.position.set(5,5,5)
+        scene.add( light )
+        light.castShadow    = true
+        light.shadowCameraNear  = 0.01
+        light.shadowCameraFar   = 15
+        light.shadowCameraFov   = 45
+        light.shadowCameraLeft  = -1
+        light.shadowCameraRight =  1
+        light.shadowCameraTop   =  1
+        light.shadowCameraBottom= -1
+        light.shadowBias    = 0.001
+        light.shadowDarkness    = 0.2
+        light.shadowMapWidth    = 1024
+        light.shadowMapHeight   = 1024
 
-    var material = $scope.selectedObject.getMaterial();
+        camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        camera.position.z = 5;
 
-    var spaceObject = new THREE.Mesh(geometry, material);
-    
-    scene.add(spaceObject);
+        renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("planetCanvas")});
+        renderer.setSize( window.innerWidth, window.innerHeight - 50 );
+        // document.getElementById('view').appendChild( renderer.domElement );
+
+    }
 
     function render() {
         requestAnimationFrame(render);
@@ -68,6 +72,8 @@ angular.module('portfolioApp').controller('threejsPlanetCtrl', ['$scope',  'plan
 
     }, false );
 
+    initScene();
+    updateObject();
     render();
 
 }]);
