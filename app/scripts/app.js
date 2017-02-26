b9bf8dae2cc4bfdfeeaa8589d6d10b23fa0136f2
Andy M <andy.dude@gmail.com>
2017-02-26 13:37:08 -0600

    Change to HTML5 Mode

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,10 @@ import module from '../app-module.js';
  */
 
 module.config([ '$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-//    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode({
+  enabled: true,
+  requireBase: false
+});
 
 
     $routeProvider
